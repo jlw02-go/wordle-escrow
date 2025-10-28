ae32458ccf6e519978c28385111338231348b94c
jlw02-go <josephlwhite@gmail.com>
2025-10-28 11:45:12 -0500

    Update App.tsx

--- wordle-escrow/App.tsx
+++ wordle-escrow/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-// FIX: Use namespace import for react-router-dom to resolve potential module resolution issues.
-import * as ReactRouterDOM from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GroupPage from './pages/GroupPage';
 import SettingsPage from './pages/SettingsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import FirebaseWrapper from './components/FirebaseWrapper';
-
-const { BrowserRouter, Routes, Route } = ReactRouterDOM;
 
 const App: React.FC = () => {
   return (
