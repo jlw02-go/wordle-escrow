fa2b4aa238a621183e9ee9ded9eefebec3351457
jlw02-go <240564249+jlw02-go@users.noreply.github.com>
2025-10-29 12:41:31 -0500

    Create currentUser.ts

---
+++ wordle-escrow/utils/currentUser.ts
@@ -0,0 +1,9 @@
+// src/utils/currentUser.ts
+const KEY = "displayName";
+
+export function getDisplayName(): string {
+  return (localStorage.getItem(KEY) || "").trim();
+}
+export function setDisplayName(name: string) {
+  localStorage.setItem(KEY, name.trim());
+}
