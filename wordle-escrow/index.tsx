8325b206bef07d01713a2eafedc7c302604c0b00
jlw02-go <josephlwhite@gmail.com>
2025-10-28 20:04:09 -0500

    Update index.tsx

--- wordle-escrow/index.tsx
+++ wordle-escrow/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// 👇 Add these imports
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// 👇 Create a single client for your app
+const queryClient = new QueryClient();
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -11,6 +17,9 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 👇 Wrap your whole app in the QueryClientProvider */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
